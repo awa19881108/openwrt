f6c551f6b7d85ec6b46259fa2b1633b49a633e72
awa19881108 <awa19881108@gmail.com>
2026-02-01 02:47:30 +0800

    Add diy-part1.sh for helloworld feed
    Removed old helloworld feed to avoid conflicts and added a new helloworld source. Updated golang package to a newer version.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# DIY script part 1 - Before Update feeds
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 移除舊 helloworld（避免衝突）
+sed -i '/helloworld/d' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 加 sbwml 的 helloworld v5 分支（支援 24.10 的 ssr-plus）
+echo "src-git helloworld https://github.com/sbwml/openwrt_helloworld.git^v5" >> feeds.conf.default
+
+# 更新 golang（xray 等需要較新版本）
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
