e9a0046cdbe8691e008d73c66b406e728ccfae5b
awa19881108 <awa19881108@gmail.com>
2026-02-01 17:46:34 +0800

    Add mosdns and passwall feeds to configurationAdd mosdns feed to fix dependency warning for luci-app-ssr-plus

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,6 +7,13 @@ sed -i '/helloworld/d' feeds.conf.default
 # 加 sbwml 的 helloworld v5 分支（支援 24.10 的 ssr-plus）
 echo "src-git helloworld https://github.com/sbwml/openwrt_helloworld.git^v5" >> feeds.conf.default
 
+# 加 mosdns feed（sbwml 專用，支援 ssr+）
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git^v5" >> feeds.conf.default
+
+# 如果你加了 passwall2，也保持
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+
 # 更新 golang（xray 等需要較新版本）
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
