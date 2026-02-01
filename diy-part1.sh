a7d958356d5a343f14e594b62954c54152ff4dbe
awa19881108 <awa19881108@gmail.com>
2026-02-01 18:17:34 +0800

    Add sbwml mosdns feed to fix ssr-plus dependency

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,10 @@ echo "src-git helloworld https://github.com/sbwml/openwrt_helloworld.git^v5" >> 
 # 加 mosdns feed（sbwml 專用，支援 ssr+）
 echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git^v5" >> feeds.conf.default
 
+# 專門加 mosdns（sbwml 自己的 mosdns feed，與 helloworld 完美配合）
+sed -i '/mosdns/d' feeds.conf.default
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git^v5" >> feeds.conf.default
+
 # 如果你加了 passwall2，也保持
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
